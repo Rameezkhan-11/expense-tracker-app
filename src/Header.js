f04ab9b1f3289d3cce3d10263c8f971ee52cfe83
rameez <rameezkhansadozai@gmail.com>
2021-02-10 23:56:50 +0500

    colour changed

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ function Header(){
 return(
     <>  
         <h1>Expense Tracker App<br /> TeamV8</h1>
-        <p style={{textAlign : 'center',color : 'blue'}}>By Rameez khan</p>
+        <p style={{textAlign : 'center', color : 'blue'}}>By Rameez khan</p>
         
 
     </>
