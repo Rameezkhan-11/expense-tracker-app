592fa6af6d1c621eb6e4e9769195e4579afdccf7
rameez <rameezkhansadozai@gmail.com>
2021-02-12 00:32:21 +0500

    header updated

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ function Header(){
     
 return(
     <>  
-        <h1>Expense Tracker App<br /> TeamV8</h1>
+        <h1>Expense Tracker App<br /> Rameez khan / TeamV8</h1>
         
         
         <p style={{textAlign : 'center', color : 'blue'}}>By Rameez khan</p>
